4637e1882f22c4c7639fb4431d6c644af378d6b5
Fábio Pereira <fabio.jve@gmail.com>
2017-07-12 18:05:18 -0400

    added MultiThreadLED2

---
+++ MultiThreadLED2/src/blinky_thread_entry.c
@@ -0,0 +1,36 @@
+/***********************************************************************************************************************
+* File Name    : blinky_thread_entry.c
+* Description  : This is a very simple example application that blinks all the LEDs on a board.
+***********************************************************************************************************************/
+
+#include "blinky_thread.h"
+
+extern TX_THREAD red_thread;
+
+/*******************************************************************************************************************//**
+ * @brief  Blinky example application
+ *
+ * Blinks all leds at a rate of 1 second using the the threadx sleep function.
+ * Only references two other modules including the BSP, IOPORT.
+ *
+ **********************************************************************************************************************/
+void blinky_thread_entry(void)
+{
+    uint8_t redStarted = 0;
+    /* LED type structure */
+    bsp_leds_t leds;
+    /* Get LED information for this board */
+    R_BSP_LedsGet(&leds);
+    while (1) {
+        //g_ioport.p_api->pinWrite(IOPORT_PORT_06_PIN_00, IOPORT_LEVEL_LOW);
+        g_ioport.p_api->pinWrite(leds.p_leds[0], IOPORT_LEVEL_LOW);
+        tx_thread_sleep (50);
+        //g_ioport.p_api->pinWrite(IOPORT_PORT_06_PIN_00, IOPORT_LEVEL_HIGH);
+        g_ioport.p_api->pinWrite(leds.p_leds[0], IOPORT_LEVEL_HIGH);
+        tx_thread_sleep (50);
+        if (!redStarted) {
+            redStarted = 1;
+            tx_thread_resume(&red_thread);
+        }
+    }
+}
---
+++ MultiThreadLED2/src/red_thread_entry.c
@@ -0,0 +1,15 @@
+#include "red_thread.h"
+
+/* Red Thread entry function */
+void red_thread_entry(void){
+    /* LED type structure */
+    bsp_leds_t leds;
+    /* Get LED information for this board */
+    R_BSP_LedsGet(&leds);
+    while (1){
+        g_ioport.p_api->pinWrite(leds.p_leds[1], IOPORT_LEVEL_LOW);
+        tx_thread_sleep(50);
+        g_ioport.p_api->pinWrite(leds.p_leds[1], IOPORT_LEVEL_HIGH);
+        tx_thread_sleep(50);
+    }
+}
